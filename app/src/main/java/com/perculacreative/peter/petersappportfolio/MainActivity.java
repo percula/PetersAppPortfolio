761dfa94eccb2cde097c7534de7d6b3ad8c9c81e
percula <perculacreative@gmail.com>
2016-08-25 02:21:51 +0000

    First version, no apps added yet but layout set.

--- app/src/main/java/com/perculacreative/peter/petersappportfolio/MainActivity.java
+++ app/src/main/java/com/perculacreative/peter/petersappportfolio/MainActivity.java
@@ -1,7 +1,10 @@
 package com.perculacreative.peter.petersappportfolio;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,68 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button moviesButton = (Button) findViewById(R.id.movies);
+        Button stockButton = (Button) findViewById(R.id.stock);
+        Button buildBiggerButton = (Button) findViewById(R.id.buildbigger);
+        Button materialButton = (Button) findViewById(R.id.material);
+        Button ubiquitousButton = (Button) findViewById(R.id.ubiquitous);
+        Button capstoneButton = (Button) findViewById(R.id.capstone);
+
+        moviesButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                String message = getString(R.string.toast_begin) +
+                        ((Button) view).getText() +
+                        getString(R.string.toast_end);
+                Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        stockButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                String message = getString(R.string.toast_begin) +
+                        ((Button) view).getText() +
+                        getString(R.string.toast_end);
+                Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        buildBiggerButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                String message = getString(R.string.toast_begin) +
+                        ((Button) view).getText() +
+                        getString(R.string.toast_end);
+                Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        materialButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                String message = getString(R.string.toast_begin) +
+                        ((Button) view).getText() +
+                        getString(R.string.toast_end);
+                Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        ubiquitousButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                String message = getString(R.string.toast_begin) +
+                        ((Button) view).getText() +
+                        getString(R.string.toast_end);
+                Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        capstoneButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                String message = getString(R.string.toast_begin) +
+                        ((Button) view).getText() +
+                        getString(R.string.toast_end);
+                Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
     }
 }
